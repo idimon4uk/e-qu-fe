d723de0f7250450ae826bb46ec76063142d6dc47
D.Matskevych <dimqqqq@gmail.com>
2018-10-17 15:22:27 +0300

    init coming soon...

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            coming soon ...
           </p>
           <a
             className="App-link"
@@ -17,7 +17,6 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
           </a>
         </header>
       </div>
